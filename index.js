2114e655357cebf9913c1d1cf0db612ebc85e5a2
Iheagwaram <tiheagwaram5@gmail.com>
2024-12-11 12:57:33 +0100

    arrays

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var student = false
 var me = 'tired'
 var presido = 'T-pain'
 var job = 'teacher'
+var classmate = ['Damola', 'Detumo','Alex', 'Sydney', 'Femi', 'Olakitan', 'Daniel', 'David']
+var texting = ['Peace', false, 090593, null, undefined]
 
 
 
@@ -42,19 +44,41 @@ var sentence = `There is a developer named ${dev} and he got two friends name ${
 // } else{
 //     console.log('Sapa dey for everybody');
 // }
-if(job == 'lawyer'){
-    console.log('Your bill is 200k');
+// if(job == 'lawyer'){
+//     console.log('Your bill is 200k');
     
-}else if(job == 'police'){
-    console.log('Your bill is N1m');
+// }else if(job == 'police'){
+//     console.log('Your bill is N1m');
     
-}else if(job == 'influencer'){
-    console.log('your bill is 600k');
+// }else if(job == 'influencer'){
+//     console.log('your bill is 600k');
     
-}else if(job == 'teacher'){
-    console.log('your bill is 50k');
+// }else if(job == 'teacher'){
+//     console.log('your bill is 50k');
     
-} else{
-    console.log('your bill is 100k');
+// } else{
+//     console.log('your bill is 100k');
     
-}
+// }
+// console.log(texting);
+// console.log(classmate);
+classmate.push('Ehis')
+// console.log(classmate);
+classmate.push('Samuel')
+// console.log(classmate);
+// classmate.pop()
+// console.log(classmate);
+classmate.shift()
+console.log(classmate);
+classmate.unshift('Damola')
+// console.log(classmate);
+// console.log(classmate.length);
+// console.log(classmate[0]);
+// classmate[3]= "Olakitan"
+// console.log(classmate);
+classmate[9] = "Temitayo"
+console.log(classmate);
+
+
+
+
