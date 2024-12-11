68901246443be7de3606339ffecfd8e4e1c9a6bd
Iheagwaram <tiheagwaram5@gmail.com>
2024-12-11 12:15:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var dev = 'David'
+var des = 'Sydney'
+var acct = 'Damola'
+var age = 25
+var student = false
+var me = 'tired'
+var presido = 'T-pain'
+var job = 'teacher'
+
+
+
+// concatenation
+var sentence = `There is a developer named ${dev} and he got two friends name ${des} and ${acct}`
+// console.log(sentence);
+// console.log(sentence.length);
+// console.log(sentence.toUpperCase());
+// console.log(sentence.toLowerCase());
+
+// if(age >= 18){
+// console.log('You can vote');
+
+// } else{
+//     console.log('You are too young to vote');
+// }
+
+// if(student == true){
+//     console.log('Free Tickets');
+    
+// } else{
+//     console.log('Pay your complete money');
+// }
+// if(me != 'tired'){
+//     console.log('Continu working');
+    
+// } else{
+//     console.log('Take a break and order coffee');
+    
+// }
+// if(presido != 'T-pain'){
+//     console.log('Life is good');
+    
+// } else{
+//     console.log('Sapa dey for everybody');
+// }
+if(job == 'lawyer'){
+    console.log('Your bill is 200k');
+    
+}else if(job == 'police'){
+    console.log('Your bill is N1m');
+    
+}else if(job == 'influencer'){
+    console.log('your bill is 600k');
+    
+}else if(job == 'teacher'){
+    console.log('your bill is 50k');
+    
+} else{
+    console.log('your bill is 100k');
+    
+}
